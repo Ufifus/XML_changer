52243a3ae7c10a730b9e6757c7e6ba643a6e01f9
EdwFa <45067786+EdwFa@users.noreply.github.com>
2022-09-14 20:06:11 +0300

    update code

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import pandas as pd
 # from io import StringIO
 # from lxml.etree import XML, XSLT, parse
 
+import lxml.etree as ETT
+
+
 def keys_by_depth(dict_, depth=0, output=None):
     if output is None:
         output = {}
@@ -32,51 +35,74 @@ def main():
     st.set_page_config(layout = 'wide',)
     doc_type = st.sidebar.selectbox(
         "Выбор документа",
-        ("Выписной эпикриз короткий", "Выписной эпикриз полный")
+        ("Выписной эпикриз короткий", "Выписной эпикриз полный", "Приложение_Д", "Приложение_Е")
     )
 
     path = os.path.abspath('data/epi_amb/')  # название папки с файлами
     if(doc_type == 'Выписной эпикриз короткий'):
         file_name = 'CDADocumentRuAmbulatorySummury_min.xml'
+    elif(doc_type == 'Приложение_Д'):
+        file_name = 'pril_D.xml'
+    elif(doc_type == 'Приложение_Е'):
+        file_name = 'pril_E.xml'
     else:
         file_name = 'CDADocumentRuAmbulatorySummury_max.xml'
+
     file_xml = os.path.join(path, file_name)
     # tree = ET.parse(os.path.join(path, file_name))
 
     # Проверка xsd
     with st.expander("Валидация схем по .xsd"):
         xsd_file_name = 'CDA.xsd'
-        schema_root = etree.parse(os.path.join(path,xsd_file_name))
+        try:
+            schema_root = etree.parse(os.path.join(path, xsd_file_name))
+        except ValueError:
+            st.error(ValueError)
         # st.write(schema_root)
         schema = etree.XMLSchema(schema_root)
         # st.write(schema)
         xml = etree.parse(file_xml)
         if not schema.validate(xml):
-            st.error("xml-файл содержит ошибки и не соответсвует xsd-схеме. Протокол ошибок ниже :" )
-            st.error(schema.error_log)
+            try:
+                st.error("xml-файл содержит ошибки и не соответсвует xsd-схеме. Протокол ошибок ниже :" )
+                st.error(schema.error_log)
+            except ValueError:
+                st.error(ValueError)
         else:
             st.success("xml-файл не содержит ошибок")
 
-    col1, col2 = st.columns([1, 3])
-    col1.info("Структура документа по уровням")
-    col2.success("Содержание. Клинический документ: CD.Эпикриз-выписной.Законченный ")
+    with st.expander("XSL - ПРЕОБРАЗОВАНИЕ"):
+        xsl_file_name = 'AmbSum.xsl'
+        try:
+            xslt = ETT.parse(os.path.join(path, xsl_file_name))
+        except ValueError:
+            st.error(ValueError)
+        dom = ETT.parse(file_xml)
+        # xslt = ET.parse(file_xsl)
+        transform = ETT.XSLT(xslt)
+        newdom = transform(dom)
+
+        html = ETT.tostring(newdom, pretty_print=True)
+        st.components.v1.html(html, width=None, height=960, scrolling=True)
+
+        # st.write(ETT.tostring(newdom, pretty_print=True))
+
+
+    # col1, col2 = st.columns([1, 3])
+    st.sidebar.info("Структура документа по уровням")
+    st.success("Содержание. Клинический документ: CD.Эпикриз-выписной.Законченный ")
 
     fileptr = open(file_xml, encoding="utf8")
     # read xml content from the file
     xml_content = fileptr.read()
-    print("XML content is:")
-    print(xml_content)
+    # print("XML content is:")
+    # print(xml_content)
 
     xml_dict = xmltodict.parse(xml_content)
     keys = keys_by_depth(xml_dict)
-    print('Ключи - '+str(keys))
-    col1.write(keys)
-    col2.write(xml_dict)
-
-
-
-
-
+    # print('Ключи - '+str(keys))
+    st.sidebar.write(keys)
+    st.write(xml_dict)
 
 if __name__ == '__main__':
     main()
